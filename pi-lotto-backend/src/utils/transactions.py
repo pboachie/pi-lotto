f76d7bf019d8ea0c0eae5f46e2a0d6480f027047
pboachie <princeboachie@gmail.com>
2024-05-11 13:03:45 -0700

    chore: Import Annotated from typing_extensions in transactions.py
    This commit imports the `Annotated` class from the `typing_extensions` module in the `transactions.py` file. This class is used to provide type hints with additional metadata.
    
    The purpose of this change is to enhance the type annotations in the codebase by using the `Annotated` class for more descriptive type hints.

--- pi-lotto-backend/src/utils/transactions.py
+++ pi-lotto-backend/src/utils/transactions.py
@@ -1,6 +1,6 @@
 # src/utils/transactions.py
-
 from typing import Optional
+from typing_extensions import Annotated
 from fastapi import Depends, HTTPException, status
 from jose import JWTError, jwt
 from src.db.database import get_db
