e44f10a408b1369a129d01217972efbbb04ec62a
pboachie <princeboachie@gmail.com>
2024-05-11 13:03:33 -0700

    chore: Update auth.py to include DEV_DOCS_PASSWORD

--- pi-lotto-backend/src/auth.py
+++ pi-lotto-backend/src/auth.py
@@ -1,5 +1,5 @@
 # src/auth.py
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from src.utils.utils import load_config
 
 config = load_config()
@@ -7,6 +7,7 @@ config = load_config()
 SECRET_KEY = config['jwt']['secret_key']
 ALGORITHM = config['jwt']['algorithm']
 ACCESS_TOKEN_EXPIRE_MINUTES = config['jwt']['access_token_expire_minutes']
+DEV_DOCS_PASSWORD = config['dev_docs']['password']
 
 # OAuth2 scheme for authentication
 OAUTH2_SCHEME = OAuth2PasswordBearer(tokenUrl="token")
