47beed18140dd7590273bcd8b0f2b2eb315f8cdb
pboachie <princeboachie@gmail.com>
2024-05-11 17:05:15 -0700

    chore: Remove commented out code and unused imports
    This commit removes commented out code and unused imports in the `main.py` file. The code changes improve code readability and maintainability by removing unnecessary code.

--- pi-lotto-backend/main.py
+++ pi-lotto-backend/main.py
@@ -1,7 +1,7 @@
 # main.py
 
 import multiprocessing
-import spacy
+# import spacy
 from fastapi import FastAPI, Request, status
 from fastapi.routing import APIRouter
 from src.utils.utils import logging, JSONResponse
@@ -45,56 +45,56 @@ async def read_root():
 async def loaderio_verification():
     return 'loaderio-28b24b7ab3f2743ac5e4b68dcdf851bf'
 
-def serve(
-    model: str,
-    #   api_key: str = typer.Option(NO_API_KEY, prompt=True, hide_input=True, show_default=True, confirmation_prompt=True),
-    host: str = config['app']['host'],
-    port: int = config['app']['port'],
-    use_gunicorn: bool = False,
-    n_workers: int = (multiprocessing.cpu_count() * 2) + 1,
-):
+# def serve(
+#     model: str,
+#     #   api_key: str = typer.Option(NO_API_KEY, prompt=True, hide_input=True, show_default=True, confirmation_prompt=True),
+#     host: str = config['app']['host'],
+#     port: int = config['app']['port'],
+#     use_gunicorn: bool = False,
+#     n_workers: int = (multiprocessing.cpu_count() * 2) + 1,
+# ):
 
-    import uvicorn
-    from starlette.requests import Request
+#     import uvicorn
+#     from starlette.requests import Request
 
-    nlp = spacy.load(model)
+#     nlp = spacy.load(model)
 
-    @app.middleware("http")
-    async def update_request_state(request: Request, call_next):
-        request.state.nlp = nlp
-        # request.state.api_key = api_key
-        response = await call_next(request)
-        return response
+#     @app.middleware("http")
+#     async def update_request_state(request: Request, call_next):
+#         request.state.nlp = nlp
+#         # request.state.api_key = api_key
+#         response = await call_next(request)
+#         return response
 
-    if use_gunicorn:
-        from gunicorn.app.wsgiapp import WSGIApplication
+#     if use_gunicorn:
+#         from gunicorn.app.wsgiapp import WSGIApplication
 
-        class FastAPIApplication(WSGIApplication):
-            def __init__(self, app, options=None):
-                self.options = options or {}
-                self.application = app
-                super().__init__()
+#         class FastAPIApplication(WSGIApplication):
+#             def __init__(self, app, options=None):
+#                 self.options = options or {}
+#                 self.application = app
+#                 super().__init__()
 
-            def load_config(self):
-                config = {
-                    key: value
-                    for key, value in self.options.items()
-                    if key in self.cfg.settings and value is not None
-                }
-                for key, value in config.items():
-                    self.cfg.set(key.lower(), value)
+#             def load_config(self):
+#                 config = {
+#                     key: value
+#                     for key, value in self.options.items()
+#                     if key in self.cfg.settings and value is not None
+#                 }
+#                 for key, value in config.items():
+#                     self.cfg.set(key.lower(), value)
 
-            def load(self):
-                return self.application
+#             def load(self):
+#                 return self.application
 
-        options = {
-            "bind": f"{host}:{port}",
-            "workers": n_workers,
-            "worker_class": "uvicorn.workers.UvicornWorker",
-        }
-        FastAPIApplication(app, options).run()
-    else:
-        uvicorn.run(app, host=host, port=port)
+#         options = {
+#             "bind": f"{host}:{port}",
+#             "workers": n_workers,
+#             "worker_class": "uvicorn.workers.UvicornWorker",
+#         }
+#         FastAPIApplication(app, options).run()
+#     else:
+#         uvicorn.run(app, host=host, port=port)
 
 if __name__ == "__main__":
     import uvicorn
