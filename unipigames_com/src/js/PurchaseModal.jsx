fb17eb779fe99fd2ca94dde23512198eb6bc9ce6
pboachie <princeboachie@gmail.com>
2024-05-12 19:01:49 -0700

    chore: Update API endpoints to use production URL

--- unipigames_com/src/js/PurchaseModal.jsx
+++ unipigames_com/src/js/PurchaseModal.jsx
@@ -26,46 +26,44 @@ function PurchaseModal({ numberSets = [], ticketNumber, ticketPrice, baseFee, se
 
   return ReactDOM.createPortal(
     <div className="modal-overlay">
-      <div
-        className="modal-content ticket-modal card"      >
+      <div className="modal-content ticket-modal">
         <div className="ticket-header">
-              <div className="ticket-pilotto-label">PiLotto</div>
-              <div className="ticket-PiLotto-symbol">π</div>
-            </div>
-            <div className="ticket-numbers-section">
-              {numberSets.length > 0 ? (
-                numberSets.map((set, index) => (
-                  <div key={index} className="ticket-number-set">
-                    {set.numbers.map((number, numberIndex) => (
-                      <span key={numberIndex} className="ticket-number">
-                        {number}
-                      </span>
-                    ))}
-                    <span className="ticket-PiLotto">{set.PiLotto}</span>
-                  </div>
-                ))
-              ) : (
-                <p>No number sets available.</p>
-              )}
-            </div>
-            <div className="ticket-details-section">
-              <p className="ticket-number-label">Ticket#: {ticketNumber}</p>
-              <div className="ticket-cost-section">
-                <p>Ticket Price: {ticketPrice} π</p>
-                <p>Base Fee: {baseFee} π</p>
-                <p>Service Fee: {serviceFee} π</p>
-                <p>Total Cost: {totalCost} π</p>
+          <div className="ticket-pilotto-label">PiLotto</div>
+          <div className="ticket-PiLotto-symbol">π</div>
+        </div>
+        <div className="ticket-numbers-section">
+          {numberSets.length > 0 ? (
+            numberSets.map((set, index) => (
+              <div key={index} className="ticket-number-set">
+                {set.numbers.map((number, numberIndex) => (
+                  <span key={numberIndex} className="ticket-number">
+                    {number}
+                  </span>
+                ))}
+                <span className="ticket-PiLotto">{set.PiLotto}</span>
               </div>
-            </div>
-            <div className="ticket-barcode-section">
-              <svg ref={barcodeRef} />
-            </div>
+            ))
+          ) : (
+            <p>No number sets available.</p>
+          )}
+        </div>
+        <div className="ticket-details-section">
+          <p className="ticket-number-label">Ticket#: {ticketNumber}</p>
+          <div className="ticket-cost-section">
+            <p>Ticket Price: {ticketPrice} π</p>
+            <p>Base Fee: {baseFee} π</p>
+            <p>Service Fee: {serviceFee} π</p>
+            <p>Total Cost: {totalCost} π</p>
           </div>
-          <div className="card-back"></div>
+        </div>
+        <div className="ticket-barcode-section">
+          <svg ref={barcodeRef} />
+        </div>
         <button className="close-button" onClick={onClose}>
           Close
         </button>
-      </div>,
+      </div>
+    </div>,
     document.body
   );
 }
