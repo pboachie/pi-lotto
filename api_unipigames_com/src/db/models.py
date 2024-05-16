2542ba91c26736209cff838df43b015616cacaf3
pboachie <princeboachie@gmail.com>
2024-05-15 21:42:31 -0700

    chore: Import datetime module in models.py

--- api_unipigames_com/src/db/models.py
+++ api_unipigames_com/src/db/models.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker, Session
 from sqlalchemy.sql import func
 from pydantic import BaseModel
+import datetime
 
 Base = declarative_base()
 
